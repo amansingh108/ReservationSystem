80b8398dd8fa280b7cd3afd3062cd00ebe6a040b
whaatxd2 <khoidinh06@yahoo.com>
2017-02-02 12:47:52 -0800

    Create ReservationSystem.cpp

---
+++ ReservationSystem.cpp
@@ -0,0 +1,146 @@
+//inputs: NumberOfRows, NumberOfSeats, rowSelection, seatSelection, answer
+//output: '-', message if choice is not availaible
+// program to reserve seats and will output appropriate message if inputs are not valid.
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+// Function declarations (prototypes)
+char ** CreateArrayOfSeats(int NumberOfRows, int SeatsOnRow);
+void InitializeSeats(char **ArrayOfSeats, int NumberOfRows, int SeatsOnRow);
+void DisplayArrayOfSeats(char **ArrayOfSeats, int NumberOfRows, int SeatsOnRow);
+void MemoryCleanup(char **ArrayOfSeats, int NumberOfRows, int SeatsOnRow);
+
+int counter = 0;
+int main(int argc, char* argv[])
+{
+	char **ArrayOfSeats;
+	int NumberOfRows;
+	int NumberOfSeats;
+	char answer;    // user input for "Do you want another seat?"
+	
+	// get the number of NumberOfRows and SeatsOnRow from the user
+	cout << "Enter the number of NumberOfRows: ";
+	cin >> NumberOfRows;
+	cout << "Enter the number of seats on each row: ";
+	cin >> NumberOfSeats;
+
+	ArrayOfSeats = CreateArrayOfSeats(NumberOfRows, NumberOfSeats);
+	InitializeSeats(ArrayOfSeats, NumberOfRows, NumberOfSeats);
+	DisplayArrayOfSeats(ArrayOfSeats, NumberOfRows, NumberOfSeats);
+
+	do
+	{
+		int  rowSelection;  // 1 to max NumberOfRows, input from the user
+		char seatSelection; // 'A' to max seats, input from the user, convert to a number
+		int row;    // index into ArrayOfSeats, 0 to NumberOfRows-1
+		int seat;   // index into ArrayOfSeats, 0 to seats-1
+		int total = NumberOfRows * NumberOfSeats;
+		
+		
+
+		cout << "Enter a seat selection (example 5F): ";
+		cin >> rowSelection;       // get row from the user
+		cin >> seatSelection;      // get the seat from the user
+		seatSelection = toupper(seatSelection); // convert to upper case
+		row = rowSelection - 1;     // count from zero to work with the array
+		seat = seatSelection - 'A'; // convert 'A' to 0, 'B' to 1, 'C' to 2, etc.
+
+		// Verify that a valid row and seat were entered
+		if (row >= NumberOfRows && seat >= NumberOfSeats)
+		{
+			cout << "Invalid row and seat number. Try again." << endl;
+		}
+		else if (row >= NumberOfRows)
+		{
+			cout << "Invalid row.try again." << endl;
+			
+		}
+		else if (seat >= NumberOfSeats)
+		{
+			cout << "Invalid seat number." << endl;
+			
+		}
+		else if (ArrayOfSeats[row][seat] == '-')
+		{
+			cout << "This space has already been used, try again" << endl;
+
+		}
+		else
+		{
+
+			ArrayOfSeats[row][seat] = '-';
+
+			counter++;
+		}
+
+
+		// See if the seat selection is already taken (see if it has an '-')
+		//   if it has an '-', display a message that the seat is taken
+		//   else, put an '-' in that location
+		
+		
+		if (total == counter)
+		{
+			cout << "no  more seats" << endl;
+			DisplayArrayOfSeats(ArrayOfSeats, NumberOfRows, NumberOfSeats);
+			break;
+		}
+
+
+
+		DisplayArrayOfSeats(ArrayOfSeats, NumberOfRows, NumberOfSeats);
+		cout << "Do you want to select another seat (y/n)? ";
+		cin >> answer;
+		answer = toupper(answer);
+	} while (answer == 'Y');
+
+	MemoryCleanup(ArrayOfSeats, NumberOfRows, NumberOfSeats);   // return the memory that was allocated
+
+	cout << "Press the ENTER key to continue...";
+	char buff[100];
+	cin.getline(buff, 100);
+	return 0;
+}
+
+char **CreateArrayOfSeats(int NumberOfRows, int SeatsOnRow)    // ** means pointer to pointers
+{
+	char **ArrayOfSeats;
+	ArrayOfSeats = new char*[NumberOfRows];               // create an array of pointers for the NumberOfRows
+	for (int r = 0; r < NumberOfRows; r++)
+		ArrayOfSeats[r] = new char[SeatsOnRow];   // create an array of SeatsOnRow for each row
+	return ArrayOfSeats;                     // return pointer to the array back to the main program
+}
+
+void InitializeSeats(char **ArrayOfSeats, int NumberOfRows, int SeatsOnRow)
+{
+	for (int r = 0; r<NumberOfRows; r++)          // initialize the data for each row
+	{
+		for (int s = 0; s<SeatsOnRow; s++)
+			ArrayOfSeats[r][s] = 'A' + s;    // put 'A' 'B' 'C' etc in each row
+	}
+}
+
+void DisplayArrayOfSeats(char **ArrayOfSeats, int NumberOfRows, int NumberOfSeats)
+{
+	for (int r = 0; r<NumberOfRows; r++)              // for each row
+	{
+		cout.width(2);
+		cout << r + 1 << ' ';                 // Display row numbers starting from 1
+		for (int s = 0; s<NumberOfSeats; s++)
+			cout << ArrayOfSeats[r][s] << ' ';   // Display info for each seat
+		cout << endl;                       // new line after each row
+	}
+}
+
+void MemoryCleanup(char **ArrayOfSeats, int NumberOfRows, int NumberOfSeats)
+{
+	for (int r = 0; r<NumberOfRows; r++)
+		delete[] ArrayOfSeats[r];   // delete each row of seats individually
+	delete[] ArrayOfSeats;          // delete the row array
+}
+
+
